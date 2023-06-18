428b93d258dfae7bf991b1713dc7bc24ba61f593
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-19 02:15:09 +0530

    added logic for markdown text

--- src/components/MarkedInput.jsx
+++ src/components/MarkedInput.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./MarkedInput.css";
+import editorContext from "../editorContext";
 
 const MarkedInput = () => {
+  //globally acessing/updating markdown text
+  const { setMarkdownText } = useContext(editorContext);
+  //handling user input
+  const onInputChangeHander = (e) => {
+    const enteredText = e.target.value;
+    setMarkdownText(enteredText);
+  };
+
   return (
     <div className="container">
       <div className="title">Input Here</div>
-      <textarea className="textarea-container" placeholder="Write Input"></textarea>
+      <textarea
+        className="textarea-container"
+        placeholder="Write Input"
+        onChange={onInputChangeHander}
+      ></textarea>
     </div>
   );
 };
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Result.css";
+import ReactMarkdown from "react-markdown";
+import editorContext from "../editorContext";
 
 const Result = () => {
+  //outputting the markdown text
+  const { markdownText } = useContext(editorContext);
+
   return (
     <div className="container">
       <div className="title">Output</div>
-      <div className="result-area">This is the output</div>
+      <div className="result-area">
+        <ReactMarkdown>{markdownText}</ReactMarkdown>
+      </div>
     </div>
   );
 };
