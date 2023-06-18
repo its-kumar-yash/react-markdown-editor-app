8fd0edd5448b0aa8e0e67480871276fc9090708f
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-18 22:07:41 +0530

    created markdown-context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Result from "./components/Result";
 import MarkedInput from "./components/MarkedInput";
+import EditorContext from "./editorContext";
 
 function App() {
+  const [markdownText, setMarkdownText] = useState("");
+
+  //created global state for markdown text
+  const contextValue = {
+    markdownText,
+    setMarkdownText,
+  };
+
   return (
-    <div className="App" id="app-container">
-      <h1 className="title">React Mardown Editor</h1>
-      <h1 className="para">A simple md-editor in react.js</h1>
-      <div className="editor-container">
-        <MarkedInput />
-        <Result />
+    <EditorContext.Provider value={contextValue}>
+      <div className="App" id="app-container">
+        <h1 className="title">React Mardown Editor</h1>
+        <h1 className="para">A simple md-editor in react.js</h1>
+        <div className="editor-container">
+          <MarkedInput />
+          <Result />
+        </div>
       </div>
-    </div>
+    </EditorContext.Provider>
   );
 }
 
